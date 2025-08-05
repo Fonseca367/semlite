daed1ee6a0f0ae8a63317241c8a1802aab2160b6
Fonseca367 <fonsecaalessandra024@gmail.com>
2025-08-05 11:25:59 -0300

    Inclusão Atualizações SEMlite

--- semlite/cfa.py
+++ semlite/cfa.py
@@ -1,23 +1,31 @@
 from semopy import ModelMeans
+from semopy.stats import calc_stats
 from semlite.utils import validar_csv, validar_variaveis, print_sucesso, carregar_arquivo_robusto
 
-def run_cfa(data_path, factor_name, indicators):
+def run_cfa(data_path, indicators):
     try:
         validar_csv(data_path)
         df = carregar_arquivo_robusto(data_path)
-        validar_variaveis(df, indicators)
 
-        model_desc = f"{factor_name} =~ " + " + ".join(indicators)
+        for lista in indicators.values():
+            validar_variaveis(df, lista)
+
+        partes = [f"{fator} =~ {' + '.join(itens)}" for fator, itens in indicators.items()]
+        model_desc = "\n".join(partes)
 
         model = ModelMeans(model_desc)
         model.fit(df)
 
         estimates = model.inspect(std_est=True)
+        fit_indices = calc_stats(model)
+
         print_sucesso("CFA")
+        print("📊 Resultados prontos para análise.")
 
         return {
             "model_description": model_desc,
-            "estimates": estimates
+            "estimates": estimates,
+            "fit_indices": fit_indices
         }
 
     except Exception as e:
